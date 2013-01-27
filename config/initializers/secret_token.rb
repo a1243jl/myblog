419fa5793e3e34ab6d2980479b5134d4defd0b5b
chun <chun@chun-virtual-machine.(none)>
2013-01-27 20:17:39 +0900

    create blog repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7ea88eca9b2ea49b825d23fcdbdc91755a9fbcee146c9995f3705abd0982cbfb81b3cafeb58c23b780da7b2bd07b31a3f64f6b2371ae12c7b7c7e60346dd3921'
